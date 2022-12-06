bc1f93446ca0f854f6beb140ab5d95908fb544ad
Yuliya Kyrylenko <maxjulval@gmail.com>
2022-12-06 13:56:56 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+
+
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('buildStyles', function(){
+    return gulp.src('src/sass/*.+(sass|scss)')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({
+        prefix: "",
+        suffix: ".min",
+      }))
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function(){
+    gulp.watch('src/sass/*.+(sass|scss)', gulp.parallel('buildStyles'));
+    gulp.watch('src/*.html').on('change',browserSync.reload);
+
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'buildStyles'));
+
+  
